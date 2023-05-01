469fbede85c733de7548742278faaaa61ffc6450
yewon0804 <ehdnjs0804@gmail.com>
2023-05-02 01:57:13 +0900

    BOJ 11720: 숫자의 합
    https://www.acmicpc.net/problem/11720
    - Array
    - .charAt(i) - '0'

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,15 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int n = Integer.parseInt(br.readLine());
+        String s = br.readLine();
 
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += s.charAt(i) - '0';
+        }
 
+        bw.write((String.valueOf(sum)));
         bw.flush();
     }
 }
