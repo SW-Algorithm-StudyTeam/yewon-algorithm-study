ac4f3bff047aeba517c8847d7357787f80705259
yewon0804 <ehdnjs0804@gmail.com>
2023-05-02 20:12:23 +0900

    BOJ 11659: 구간 합 구하기 4
    https://www.acmicpc.net/problem/11659
    - 구간합
    - 합 배열

---
+++ src/done/BOJ1546.java
@@ -0,0 +1,31 @@
+package done;
+
+import java.io.*;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class BOJ1546 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        double[] arr = new double[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Double.parseDouble(st.nextToken());
+        }
+
+        double sum = 0;
+        Arrays.sort(arr);
+
+        for (int i = 0; i < N; i++) {
+            sum += arr[i] / arr[N - 1] * 100;
+        }
+
+        bw.write(String.valueOf(sum / N));
+        bw.flush();
+    }
+}
