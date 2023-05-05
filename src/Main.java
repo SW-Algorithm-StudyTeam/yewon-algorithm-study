c5c93add3bf6a9930c9f7f5b14e34c4eb732c2ed
yewon0804 <ehdnjs0804@gmail.com>
2023-05-05 17:05:12 +0900

    BOJ 11268: 절댓값 합
    https://www.acmicpc.net/problem/11286
    - 우선순위 큐 Priority Queue
      - 우선순위 커스터마이징 with 람다식
      - Comparator
      - heap..

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,41 @@
 import java.io.*;
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.PriorityQueue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
         int N = Integer.parseInt(br.readLine());
-        Deque<Integer> q = new ArrayDeque<>(N);
-        for (int i = N; i > 0; i--) {
-        	q.add(i); // 4, 3, 2, 1 ..
+        PriorityQueue<Integer> myQueue = new PriorityQueue<>((o1, o2) -> {
+        	// 우선순위 어떻게 커스터마이징 해볼게요 with 람다식
+        	// Comparator
+        	// compare return이..
+        	// - 양수 -> 첫 번째 매개변수가 더 큰 값으로 판단
+        	// - 0 -> 같은 값으로 판단
+        	// - 음수 -> 첫 번째 매개변수가 더 작은 값으로 판단
+        	
+        	int first_abs = Math.abs(o1);
+        	int second_abs = Math.abs(o2);
+        	if (first_abs == second_abs) { // 절대값이 같은 경우, 음수 우선
+        		return o1 > o2 ? 1 : -1;
+        	}
+        	return first_abs - second_abs; // 절대값이 작은 데이터 우선..
+        	// heap 생각해요
+        });
+        for (int i = 0; i < N; i++) {
+        	int tmp = Integer.parseInt(br.readLine());
+        	if (tmp == 0) {
+        		if (myQueue.isEmpty()) {
+        			System.out.println("0");
+        		}
+        		else {
+        			System.out.println(myQueue.poll());
+        		}
+        	}
+        	else {
+        		myQueue.add(tmp);
+        	}
         }
-        
-        while (q.size() > 1) {
-        	q.removeLast(); // 432
-        	int tmp = q.peekLast(); // 2
-        	q.addFirst(tmp); // 2432
-        	q.removeLast(); // 243
-        }
-        System.out.println(q.peek());
         br.close();
     }
 }
---
+++ src/done/BOJ2164.java
@@ -0,0 +1,26 @@
+package done;
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class BOJ2164 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        Deque<Integer> q = new ArrayDeque<>(N);
+        for (int i = N; i > 0; i--) {
+        	q.add(i); // 4, 3, 2, 1 ..
+        }
+        
+        while (q.size() > 1) {
+        	q.removeLast(); // 432
+        	int tmp = q.peekLast(); // 2
+        	q.addFirst(tmp); // 2432
+        	q.removeLast(); // 243
+        }
+        System.out.println(q.peek());
+        br.close();
+    }
+}
