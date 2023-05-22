56e531c0ff20b001a48cbb80357e893609b0fcf3
yewon0804 <ehdnjs0804@gmail.com>
2023-05-22 20:46:24 +0900

    BOJ 1929: 소수 구하기
    https://www.acmicpc.net/problem/1929
    에라토스테네스의 체

--- src/Main.java
+++ src/Main.java
@@ -1,69 +1,34 @@
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
-    static boolean[][] visited;
-    static int[][] A;
-    static int N, M;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        /*
-        * 미로 찾기
-        *
-        * '1'로 표현된 칸으로 이어진 길..
-        * (1, 1)에서 (N, M)까지 가는 최단거리 얼마일까?
-        *
-        * DFS, BFS 모두 사용 가능해요
-        * 근데? 최단거리 문제에서는 BFS가 유리합니다
-        * BFS는, 해당 깊이에서 갈 수 있는 노드 탐색을 마친 후 다음 깊이로 넘어가기 때문에..
-        * BFS로 목적지에 도달하는 경로 찾았다? 찾는 순간 그 경로가 바로 최단경로 인거예요
-        * */
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
 
-        A = new int[N][M];
-        visited = new boolean[N][M];
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            String line = st.nextToken(); // 한 줄 통째로 받아와서
-            for (int j = 0; j < M; j++) {
-                A[i][j] = Integer.parseInt(line.substring(j, j+1)); // 하나씩 끊어서 숫자 넣을게요
+        int[] arr = new int[N + 1];
+        for (int i = 2; i <= N; i++) {
+            arr[i] = i;
+        }
+
+        for (int i = 2; i <= Math.sqrt(N); i++) {
+            if (arr[i] == 0) continue;
+            for (int j = i+i; j <= N; j = j+i) {
+//                j = j+i 주목! 다음으로 넘어가는 이 로직 중요
+//                소수의 배수 값을 N까지 반복 하는거니까..
+                arr[j] = 0;
             }
         }
-        BFS(0, 0);
-        System.out.println(A[N-1][M-1]);
+
+        for (int i = M; i <= N; i++) {
+            if (arr[i] != 0) {
+                System.out.println(arr[i]);
+            }
+        }
 
         br.close();
-    }
-
-    private static void BFS(int i, int j) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {i, j});
-        visited[i][j] = true;
-        while(!queue.isEmpty()) {
-            // 더이상 BFS를 할 수 없게 될 때까지
-            int now[] = queue.poll();
-            for (int k = 0; k < 4; k++) {
-                // 상하좌우 탐색
-                int x = now[0] + dx[k];
-                int y = now[1] + dy[k];
-
-                if (x >= 0 && y >= 0 && x < N && y < M) {
-                    if (A[x][y] != 0 && !visited[x][y]) {
-                        visited[x][y] = true;
-                        A[x][y] = A[now[0]][now[1]] + 1; // *핵심*
-                        queue.add(new int[] {x, y});
-                    }
-                }
-            }
-        }
     }
 }
---
+++ src/done/BOJ2178.java
@@ -0,0 +1,73 @@
+package done;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ2178 {
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+    static boolean[][] visited;
+    static int[][] A;
+    static int N, M;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        /*
+        * 미로 찾기
+        *
+        * '1'로 표현된 칸으로 이어진 길..
+        * (1, 1)에서 (N, M)까지 가는 최단거리 얼마일까?
+        *
+        * DFS, BFS 모두 사용 가능해요
+        * 근데? 최단거리 문제에서는 BFS가 유리합니다
+        * BFS는, 해당 깊이에서 갈 수 있는 노드 탐색을 마친 후 다음 깊이로 넘어가기 때문에..
+        * BFS로 목적지에 도달하는 경로 찾았다? 찾는 순간 그 경로가 바로 최단경로 인거예요
+        * */
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        A = new int[N][M];
+        visited = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String line = st.nextToken(); // 한 줄 통째로 받아와서
+            for (int j = 0; j < M; j++) {
+                A[i][j] = Integer.parseInt(line.substring(j, j+1)); // 하나씩 끊어서 숫자 넣을게요
+            }
+        }
+        BFS(0, 0);
+        System.out.println(A[N-1][M-1]);
+
+        br.close();
+    }
+
+    private static void BFS(int i, int j) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[] {i, j});
+        visited[i][j] = true;
+        while(!queue.isEmpty()) {
+            // 더이상 BFS를 할 수 없게 될 때까지
+            int now[] = queue.poll();
+            for (int k = 0; k < 4; k++) {
+                // 상하좌우 탐색
+                int x = now[0] + dx[k];
+                int y = now[1] + dy[k];
+
+                if (x >= 0 && y >= 0 && x < N && y < M) {
+                    if (A[x][y] != 0 && !visited[x][y]) {
+                        visited[x][y] = true;
+                        A[x][y] = A[now[0]][now[1]] + 1; // *핵심*
+                        queue.add(new int[] {x, y});
+                    }
+                }
+            }
+        }
+    }
+}
